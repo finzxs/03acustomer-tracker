b670324b518da177d1f66c918df3745da6dfd30f
Hayden <haydenfinney@usf.edu>
2025-09-14 13:14:06 -0400

    Creating Customer Records

---
+++ cc_3.js
@@ -0,0 +1,13 @@
+// Creating Customer Records with endless arrays
+
+const customers = [
+    {name:"Christopher", email:"chris.kelly@micro.soft", purchases: ["hot dogs", "soda", "chips"]},
+    {name:"Amanda", email:"amanda.jones@web.mail", purchases: ["coffee", "bagel", "donut"]},
+    {name:"Jonsey", email:"jonsey.bright@domain.com", purchases: ["salad", "water", "fruit"]},
+];
+
+// Adding a new customer
+
+//Updating Customer Info
+
+//Display Customer Info
